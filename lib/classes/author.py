16c2421fadce3520dfbd92be4e06ed47ba8f4236
Brandon2401 <dikirrbrandon@gmail.com>
2025-11-18 19:34:41 +0300

    added to the author.py

---
+++ lib/classes/author.py
@@ -0,0 +1,28 @@
+from .article import Article
+
+class Author:
+    def __init__(self, name):
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string.")
+        if len(name) == 0:
+            raise valueError("Name must be longer than 0 characters.")
+        self._name = name
+       
+@property
+def name(self):
+    return self._name
+ 
+def articles(self):
+    return [article for article in Article.all if article.author == self]
+
+def magazines(self):
+    return list ({article.magazine for artilce in self.articles()})
+ 
+def add_article(self, magazine, title):
+    return Article(self,magazine,title)
+
+def topic_areas(self):
+    mags = self.magazines()
+    if not mags:
+        return None 
+    return list ({mag.categorry for mag in mags})
