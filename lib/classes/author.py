920e6b127adf690922beb2c9b0e4c1be6d9076d2
Brandon2401 <dikirrbrandon@gmail.com>
2025-11-18 22:48:31 +0300

    added code to the aritcle.py

--- lib/classes/author.py
+++ lib/classes/author.py
@@ -5,24 +5,26 @@ class Author:
         if not isinstance(name, str):
             raise TypeError("Name must be a string.")
         if len(name) == 0:
-            raise valueError("Name must be longer than 0 characters.")
+            raise ValueError("Name must be longer than 0 characters.")
         self._name = name
-       
-@property
-def name(self):
-    return self._name
- 
-def articles(self):
-    return [article for article in Article.all if article.author == self]
 
-def magazines(self):
-    return list ({article.magazine for artilce in self.articles()})
- 
-def add_article(self, magazine, title):
-    return Article(self,magazine,title)
+    @property
+    def name(self):
+        return self._name   
 
-def topic_areas(self):
-    mags = self.magazines()
-    if not mags:
-        return None 
-    return list ({mag.categorry for mag in mags})
+ 
+    def articles(self):
+        return [article for article in Article.all if article.author == self]
+
+    def magazines(self):
+        return list({article.magazine for article in self.articles()})
+
+    
+    def add_article(self, magazine, title):
+        return Article(self, magazine, title)
+
+    def topic_areas(self):
+        mags = self.magazines()
+        if not mags:
+            return None
+        return list({mag.category for mag in mags})
